c90c3c2b418f07fcdb1b1ec0d81965a1cffe903b
tw0517tw <1003146+tw0517tw@users.noreply.github.com>
2022-03-17 18:06:53 +0800

    feat: add styled-components

--- components/HeroCard.tsx
+++ components/HeroCard.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 type HeroCardHero = {
   id: string;
@@ -6,8 +7,22 @@ type HeroCardHero = {
   image: string;
 };
 
+const Card = styled.a`
+  max-width: 120px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
 const HeroCard = ({ hero }: { hero: HeroCardHero }) => {
-  return <Link href={`/heroes/${hero.id}`}>{hero.name}</Link>;
+  return (
+    <Link href={`/heroes/${hero.id}`} passHref scroll={false}>
+      <Card>
+        <img src={hero.image}></img>
+        {hero.name}
+      </Card>
+    </Link>
+  );
 };
 
 export default HeroCard;
